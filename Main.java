bbdc8c35054a0fe6b6004d835809a413437f39f7
MohitMalikGit <88261586+MohitMalikGit@users.noreply.github.com>
2024-11-19 11:44:51 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 		String arr[]  = {"Akhilesh" , "Sohali" , "Mohit" , "Ekta"};
 		for(String x : arr) System.out.println(x);
+		System.out.println("changed");
 	}
 
 }
